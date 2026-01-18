3ea6b97d4a760ec4884dd508df2fa321da4b478f
hanwn <hanwn7721@gmail.com>
2026-01-18 22:08:26 +0800

    feat: add key map

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,14 @@
 -- Insert mode: jj -> Esc
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "gi", function()
+	Snacks.picker.lsp_implementations({ picker = true })
+end, { desc = "Go to implementations" })
+
+vim.keymap.set("n", "gs", function()
+	Snacks.picker.lsp_implementations({ picker = true })
+end, { desc = "Go to super method" })
+
+vim.keymap.set("n", "gt", function()
+	Snacks.picker.lsp_type_definitions({ picker = true })
+end)
