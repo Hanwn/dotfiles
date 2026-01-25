3b9e9012d7835a5966ecb24323a10921968367f4
hanwn <hanwn7721@gmail.com>
2026-01-25 22:15:02 +0800

    :sparkles: add open code for vim

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,11 @@ return {
 	opts = function(_, opts)
 		opts.options.section_separators = { left = "", right = "" }
 		opts.options.component_separators = { left = "", right = "" }
-		opts.sections.lualine_z = {}
+		opts.sections.lualine_z = {
+			{
+				require("opencode").statusline,
+			},
+		}
 		opts.options.theme = "everforest"
 	end,
 }
