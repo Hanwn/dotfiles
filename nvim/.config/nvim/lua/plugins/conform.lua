0f36eb0468b07f88b6432e40d73b6f0b8183ad54
hanwn <hanwn7721@gmail.com>
2026-03-28 12:27:10 +0800

    refactor configuration files for improved formatting and organization

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,25 +1,43 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      json = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
-      scss = { "prettier" },
-      markdown = { "prettier" },
-      yaml = { "prettier" },
-      go = { "gofumpt", "goimports" },
-      lua = { "stylua" },
-      rust = {},
-      python = { "ruff_fix", "ruff_format" },
-      sh = { "shfmt" },
-      yaml = { "yamlfmt" },
-      toml = { "taplo" },
-      typst = { "prettypst" },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			markdown = { "prettier" },
+			yaml = { "prettier" },
+			go = { "gofumpt", "goimports" },
+			lua = { "stylua" },
+			rust = {},
+			python = { "ruff_fix", "ruff_format" },
+			sh = { "shfmt" },
+			yaml = { "yamlfmt" },
+			toml = { "taplo" },
+			typst = { "prettypst" },
+		},
+		formatters = {
+		  prettier = {
+		    options = {
+		      tab_width = 4,
+		      use_tabs = false,
+		    },
+		  },
+			ruff_format = {
+				options = {
+					indent_width = 4,
+				},
+			},
+			shfmt = {
+				options = {
+					indent = 2,
+				},
+			},
+		},
+	},
 }
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,39 +1,39 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- LSP Servers
-        "clangd",
-        "css-lsp",
-        "docker-compose-language-service",
-        "dockerfile-language-server",
-        "gopls",
-        "html-lsp",
-        "json-lsp",
-        "lua-language-server",
-        "neocmakelsp",
-        "ty",
-        "ruff",
-        "rust-analyzer",
-        "taplo",
-        "tinymist",
-        "vtsls",
-        "yaml-language-server",
-        
-        -- Formatters
-        "gofumpt",
-        "goimports",
-        "oxlint",
-        "prettier",
-        "prettypst",
-        "shfmt",
-        "stylua",
-        "yamlfmt",
-        
-        -- DAP
-        "codelldb",
-      },
-    },
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- LSP Servers
+				"clangd",
+				"css-lsp",
+				"docker-compose-language-service",
+				"dockerfile-language-server",
+				"gopls",
+				"html-lsp",
+				"json-lsp",
+				"lua-language-server",
+				"neocmakelsp",
+				"ty",
+				"ruff",
+				"rust-analyzer",
+				"taplo",
+				"tinymist",
+				"vtsls",
+				"yaml-language-server",
+
+				-- Formatters
+				"gofumpt",
+				"goimports",
+				"oxlint",
+				"prettier",
+				"prettypst",
+				"shfmt",
+				"stylua",
+				"yamlfmt",
+
+				-- DAP
+				"codelldb",
+			},
+		},
+	},
 }
