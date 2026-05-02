f1368fa5b10426f45c32b99fbd2399e694f1baeb
hanwn <hanwn7721@gmail.com>
2026-05-03 01:26:44 +0800

    chore: adapt config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,60 +1,60 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-    },
-    opts = {
-      servers = {
-        -- C/C++
-        clangd = {},
-        
-        -- Web Development
-        vtsls = {},
-        html = {},
-        cssls = {},
-        jsonls = {},
-        
-        -- Docker
-        docker_compose = {},
-        dockerfile = {},
-        
-        -- Go
-        gopls = {},
-        
-        -- Lua
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" },
-              },
-            },
-          },
-        },
-        
-        -- CMake
-        neocmakelsp = {},
-        
-        -- Rust
-        rust_analyzer = {},
-        
-        -- Python (ty includes lsp + linting)
-        ty = {},
-        
-        -- TOML
-        taplo = {},
-        
-        -- Typst
-        tinymist = {},
-        
-        -- YAML
-        yamlls = {},
-        
-        -- Protobuf
-        buf = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"mason-org/mason-lspconfig.nvim",
+		},
+		opts = {
+			servers = {
+				-- C/C++
+				clangd = {},
+
+				-- Web Development
+				vtsls = {},
+				html = {},
+				cssls = {},
+				jsonls = {},
+
+				-- Docker
+				docker_compose = {},
+				dockerfile = {},
+
+				-- Go
+				gopls = {},
+
+				-- Lua
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+						},
+					},
+				},
+
+				-- CMake
+				neocmakelsp = {},
+
+				-- Rust
+				rust_analyzer = {},
+
+				-- Python (ty includes lsp + linting)
+				ty = {},
+
+				-- TOML
+				taplo = {},
+
+				-- Typst
+				tinymist = {},
+
+				-- YAML
+				yamlls = {},
+
+				-- Protobuf
+				buf = {},
+			},
+		},
+	},
 }
