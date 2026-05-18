13871ff5f69b6cae35f7b49b6f85ba5291fb2880
hanwn <30523763+Hanwn@users.noreply.github.com>
2026-05-19 00:26:58 +0800

    :lipstick: set tmux search window in current session and beautify fzf format

--- zsh/.config/zsh/conf.d/fzf.zsh
+++ zsh/.config/zsh/conf.d/fzf.zsh
@@ -6,30 +6,63 @@ fi
 # Use ~~ as the trigger sequence instead of the default **
 export FZF_COMPLETION_TRIGGER='**'
 
-# Options to fzf command
+# Tokyo Night 配色 + 全局外观/交互
+export FZF_DEFAULT_OPTS="
+  --height=60%
+  --layout=reverse
+  --border=rounded
+  --margin=0,1
+  --info=inline-right
+  --prompt='> '
+  --pointer='▶'
+  --marker='✓'
+  --bind='ctrl-y:accept'
+  --bind='ctrl-u:preview-half-page-up'
+  --bind='ctrl-d:preview-half-page-down'
+  --bind='ctrl-/:toggle-preview'
+  --color=fg:#c0caf5,bg:#1a1b26,hl:#ff9e64
+  --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64
+  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#bb9af7
+  --color=marker:#9ece6a,spinner:#9ece6a,header:#7aa2f7
+"
+
+# Options for completion
 export FZF_COMPLETION_OPTS='--border --info=inline'
-
-# Options for path completion (e.g. vim **<TAB>)
 export FZF_COMPLETION_PATH_OPTS='--walker file,dir,follow,hidden'
-
-# Options for directory completion (e.g. cd **<TAB>)
 export FZF_COMPLETION_DIR_OPTS='--walker dir,follow'
 
+# fd 集成
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
-
 export FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
-
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# CTRL-T: 文件搜索预览
+export FZF_CTRL_T_OPTS="
+  --preview 'bat -n --color=always --line-range :500 {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+"
+
+# ALT-C: 目录跳转预览
+export FZF_ALT_C_OPTS="
+  --preview 'eza --tree --color=always --icons --level=2 {} | head -200'
+"
+
+# CTRL-R: 历史搜索增强
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}'
+  --preview-window 'up:3:hidden:wrap'
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --header 'CTRL-Y: copy to clipboard'
+"
+
 # Advanced customization of fzf options via _fzf_comprun function
-# - The first argument to the function is the name of the command.
-# - You should make sure to pass the rest of the arguments ($@) to fzf.
 _fzf_comprun() {
   local command=$1
   shift
 
   case "$command" in
-    cd)           fzf --preview 'tree -a -C {} | head -200'   "$@" ;;
+    cd)           fzf --preview 'eza --tree --color=always --icons --level=2 {} | head -200' "$@" ;;
     export|unset) fzf --preview "eval 'echo \$'{}"         "$@" ;;
     ssh)          fzf --preview 'dig {}'                   "$@" ;;
     *)            fzf --preview 'bat -n --color=always {}' "$@" ;;
@@ -41,12 +74,10 @@ _fzf_comprun() {
 # https://github.com/Aloxaf/fzf-tab/issues/474 :: important part
 autoload -Uz compinit
 compinit
-#bashcompinit
 
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
 # set descriptions format to enable group support
-# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
 zstyle ':completion:*:descriptions' format '[%d]'
 # set list-colors to enable filename colorizing
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
@@ -54,14 +85,33 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu no
 # case insensitive
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=*'
+
 # preview directory's content with eza when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -l --color=always $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza --tree --color=always --icons --level=2 $realpath | head -200'
 # custom fzf flags
-# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
 zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
-# To make fzf-tab follow FZF_DEFAULT_OPTS.
-# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+# To make fzf-tab follow FZF_DEFAULT_OPTS
 zstyle ':fzf-tab:*' use-fzf-default-opts on
 # switch group using `<` and `>`
 zstyle ':fzf-tab:*' switch-group '<' '>'
 zstyle ':fzf-tab:*' default-color $'\033[94m'
+
+# 进程补全预览
+zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview \
+  '[[ $group == "[process ID]" ]] && ps -p $word -o comm='
+
+# 环境变量预览
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
+  fzf-preview 'echo ${(P)word}'
+
+# git 补全预览
+zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview \
+  'git diff $word | delta'
+zstyle ':fzf-tab:complete:git-log:*' fzf-preview \
+  'git log --oneline --color=always $word'
+zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
+  'case "$group" in
+    "modified file") git diff $word | delta ;;
+    "recent commit object name") git log --oneline --color=always $word ;;
+    *) git log --oneline --color=always $word ;;
+  esac'
