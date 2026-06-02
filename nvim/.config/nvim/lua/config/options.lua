6824a9ea310725e6a278e01846cf87afd3bbca9f
hanwn <hanwn7721@gmail.com>
2026-06-03 00:47:25 +0800

    :wrench: config block cursor in nvim and set font and font size

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,10 @@ opt.shiftwidth = 4
 opt.expandtab = true
 
 vim.g.lazyvim_picker = "fzf"
+
+-- 设置光标形状：所有模式都使用 block
+vim.opt.guicursor = {
+  "n-v-c-sm:block",  -- 普通、可视、命令行、选择模式使用 block
+  "i-ci-ve:block",    -- 插入、CI、可视模式使用 block
+  "r-cr-o:hor20",     -- 替换、CR、操作符模式使用水平条
+}
