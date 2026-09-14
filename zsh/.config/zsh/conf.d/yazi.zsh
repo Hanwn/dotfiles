b188012f089651173e687d1a5702a415a76ef8fa
hanwn <hanwn7721@gmail.com>
2026-09-14 12:47:44 +0800

    fix: use yazi default script

--- zsh/.config/zsh/conf.d/yazi.zsh
+++ zsh/.config/zsh/conf.d/yazi.zsh
@@ -1,13 +1,7 @@
 function y() {
-  (( $+commands[yazi] )) || return 127
-
-  local tmp cwd
-  tmp="$(mktemp "${TMPDIR:-/tmp}/yazi-cwd.XXXXXX")" || return
-  trap 'rm -f -- "$tmp"' RETURN
-
-  command yazi "$@" --cwd-file="$tmp" || return
-  if [[ -r "$tmp" ]]; then
-    IFS= read -r -d '' cwd < "$tmp"
-    [[ "$cwd" != "$PWD" && -d "$cwd" ]] && builtin cd -- "$cwd"
-  fi
+	local tmp cwd; tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd" || builtin true
+	command rm -f -- "$tmp"
 }
